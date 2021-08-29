b25ef9abca552f37bc1f3ce34dfbc5f898dd6a28
Bruno <bruno.smsouza@gmail.com>
2021-08-29 18:50:46 -0300

    Primeiro Commit

---
+++ EggERP/src/main/java/com/eggerp/EggErpApplication.java
@@ -0,0 +1,13 @@
+package com.eggerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggErpApplication.class, args);
+	}
+
+}
